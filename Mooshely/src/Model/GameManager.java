e92b1f301e057b263a972216d7f0db6cfced732d
peboson <turner.1019@osu.edu>
2014-10-04 20:36:32 -0400

    mooshely with hops

--- Mooshely/src/Model/GameManager.java
+++ Mooshely/src/Model/GameManager.java
@@ -16,8 +16,14 @@ public class GameManager extends GameView {
     Image image = this.ii.getImage();
     int width = this.image.getWidth(null);
     int height = this.image.getHeight(null);
+    ImageIcon ij = new ImageIcon(this.getClass().getResource("/gamedesk.png"));
+    Image desk = this.ij.getImage();
     private int x = 0, y = 500 - this.height;
-    int jumpheight = 30, j;
+    double yVelocity;
+    boolean jump1, jump2;
+    final double g = .3;
+    double time;
+    int worldSizeX = 1300, viewSizeX = 800, viewSizeY = 600;
 
     public GameManager(GameViewFactory viewFactory) {
         super(viewFactory);
@@ -31,53 +37,99 @@ public class GameManager extends GameView {
     @Override
     public void onUpdate() {
         if (this.getViewFactory().getGameEngine().getKeyBoardListener()
-                .isKeyPressed(KeyEvent.VK_LEFT)) {
+                .isKeyPressed(KeyEvent.VK_LEFT)
+                && this.x - 2 >= 200) {
             this.x -= 2;
         }
         if (this.getViewFactory().getGameEngine().getKeyBoardListener()
-                .isKeyPressed(KeyEvent.VK_RIGHT)) {
+                .isKeyPressed(KeyEvent.VK_RIGHT)
+                && this.x + 2 <= 1150 - 6 - this.width) {
             this.x += 2;
         }
         if (this.getViewFactory().getGameEngine().getKeyBoardListener()
                 .isKeyPressed(KeyEvent.VK_UP)) {
-            while (this.j < this.jumpheight) {
-                this.y--;
-                try {
-                    Thread.sleep(5);
-                } catch (InterruptedException e1) {
+            this.jump1 = true;
+            this.jump2 = false;
+            this.yVelocity = 8;
+            this.time = .01;
+            while (this.jump1) {
+                //double jump
+                if (this.getViewFactory().getGameEngine().getKeyBoardListener()
+                        .isKeyPressed(KeyEvent.VK_A)
+                        && this.jump2 == false) {
+                    this.jump2 = true;
+                    this.yVelocity = 8;
                 }
-                this.repaint();
-                this.j++;
+                //moving while jumping
+                if (this.getViewFactory().getGameEngine().getKeyBoardListener()
+                        .isKeyPressed(KeyEvent.VK_LEFT)
+                        && this.x - 2 >= 200) {
+                    this.x -= 2;
+                }
+                if (this.getViewFactory().getGameEngine().getKeyBoardListener()
+                        .isKeyPressed(KeyEvent.VK_RIGHT)
+                        && this.x + 2 <= 1150 - 6 - this.width) {
+                    this.x += 2;
+                }
+
+                //boundaries
+                if ((int) (this.y - this.yVelocity * this.time - .5 * this.g
+                        * this.time * this.time) >= 500 - this.height) {
+                    this.jump1 = false;
+                    this.jump2 = false;
+                    this.y = 500;
+                } else {
+                    this.y = (int) (this.y - this.yVelocity * this.time - .5
+                            * this.g * this.time * this.time);
+                }
+
+                this.time += .01;
+                this.yVelocity -= this.g * this.time;
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
             }
         }
-        this.j = 0;
 
-        if (this.x >= 794 - this.width) {
-            this.x = 794 - this.width;
+        if (this.x >= this.worldSizeX - this.width - 6) {
+            this.x = this.worldSizeX - this.width - 6;
         }
-        if (this.x <= 0) {
-            this.x = 0;
+        if (this.x <= 200) {
+            this.x = 200;
         }
-        if (this.y >= 505 - this.height) {
-            this.y = 505 - this.height;
-        }
-        if (this.y <= 0) {
-            this.y = 0;
-        }
-        if (this.y < 505 - this.height) {
-            this.y += 1;
+        if (this.y >= 500 - this.height) {
+            this.y = 500 - this.height;
         }
     }
 
     @Override
     public void onDraw(Graphics2D g2d) {
-        g2d.setColor(Color.CYAN);
-        g2d.fillRect(0, 0, 800, 600);
+        //moving camera
+        int camX = this.x - this.viewSizeX / 2;
+        if (camX > this.worldSizeX - this.viewSizeX) {
+            camX = this.worldSizeX - this.viewSizeX;
+        }
+        if (camX < 0) {
+            camX = 0;
+        }
+        g2d.translate(-camX, 0);
+
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, this.worldSizeX, this.viewSizeY);
+//        g2d.setColor(Color.BLACK);
+//        g2d.fillRect(20, 400, 20, 100);
+//        g2d.fillRect(120, 400, 20, 100);
+//        g2d.fillRect(420, 400, 20, 100);
+//        g2d.fillRect(820, 400, 20, 100);
+
+        g2d.drawImage(this.desk, 0, 190, null);
         g2d.drawImage(this.image, this.x, this.y, null);
-        g2d.setColor(Color.gray);
-        g2d.fillRect(0, 500, 800, 100);
-        g2d.setColor(Color.GREEN);
-        g2d.fillRect(0, 500, 800, 10);
+
+//        g2d.setColor(Color.gray);
+//        g2d.fillRect(0, 500, this.worldSizeX, 100);
+//        g2d.setColor(Color.GREEN);
+//        g2d.fillRect(0, 500, this.worldSizeX, 10);
     }
 
 }
